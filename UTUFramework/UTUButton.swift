53a543ab2c8b324dda00addd20dd2d9ffe80990b
Lakshmana Sattineedi <lakshmana.sattineedi@utu.global>
2019-10-23 18:13:24 +0530

    Implemented 12621(Button Refinements)

--- UTUFramework/UTUButton.swift
+++ UTUFramework/UTUButton.swift
@@ -31,5 +31,30 @@ public class UTUButton: UIButton {
         button.setTitle(title, for: .normal)
         return button
     }
+    public convenience override init(frame : CGRect) {
+        self.init(frame: frame, label : "Logic", outlined : true, disabled: false, size: "small")
+    }
+    required init(frame : CGRect, label: String, outlined: Bool, disabled: Bool, size: String) {
+        super.init(frame: frame)
+        backgroundColor = UIColor(red: 6.0/255.0, green: 179.0/255.0, blue: 152.0/255.0, alpha: 1)
+        setTitle(label, for: .normal)
+        layer.cornerRadius = 3.0
+        layer.borderWidth = 0.5
+        titleLabel?.font = UIFont(name: "HelveticaNeue", size: 16)
+        setTitleColor(.white, for: .normal)
+        layer.borderColor = UIColor(red: 6.0/255.0, green: 179.0/255.0, blue: 152.0/255.0, alpha: 1).cgColor //6,179,152
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    
 }
 
+/*UTUButton(
+    label: String,      | "Action" | text to display in the button
+    outlined: Boolean,  | false | if true display the button in outlined mode
+    disabled: Boolean,  | false | if true button appears in disabled state
+    pressed: Boolean,   | false | if true button appears in pressed state
+    size: String,       | "regular" | "small", "regular", "big" | choose a button size
+)*/
