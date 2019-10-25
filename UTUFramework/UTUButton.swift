cf1a08d810d42b609c68b7a1d522fad1e0d93794
Lakshmana Sattineedi <lakshmana.sattineedi@utu.global>
2019-10-25 13:17:47 +0530

    Added NotoSans-Bold

--- UTUFramework/UTUButton.swift
+++ UTUFramework/UTUButton.swift
@@ -9,9 +9,10 @@
 import Foundation
 import UIKit
 
-let kSmall   =  13
-let kRegular =  16
-let kBig     =  20
+let kSmall   =  14.0
+let kRegular =  14.0
+let kBig     =  16.0
+let kFontRegular = "NotoSans-Bold"
 
 public class UTUButton: UIButton {
     public  override init(frame : CGRect) {
@@ -23,13 +24,25 @@ public class UTUButton: UIButton {
     
     required convenience init(frame : CGRect, label: String, outlined: Bool, disabled: Bool, size: String, isPress : Bool) {
         self.init(frame : frame)
-        backgroundColor = UIColor(red: 6.0/255.0, green: 179.0/255.0, blue: 152.0/255.0, alpha: 1)
         setTitle(label, for: .normal)
         layer.cornerRadius = 3.0
-        layer.borderWidth = 0.5
-        titleLabel?.font = UIFont(name: "HelveticaNeue", size: 16)
-        setTitleColor(.white, for: .normal)
-        layer.borderColor = UIColor(red: 6.0/255.0, green: 179.0/255.0, blue: 152.0/255.0, alpha: 1).cgColor //6,179,152
+        if size == "small" {
+            titleLabel?.font = UIFont(name: kFontRegular, size: CGFloat(kSmall))
+        }else if size == "regular" {
+            titleLabel?.font = UIFont(name: kFontRegular, size: CGFloat(kRegular))
+        }else if size == "big" {
+            titleLabel?.font = UIFont(name: kFontRegular, size: CGFloat(kBig))
+        }
+        if outlined {
+            layer.borderColor = UIColor(red: 6.0/255.0, green: 179.0/255.0, blue: 152.0/255.0, alpha: 1).cgColor //6,179,152
+            setTitleColor(UIColor(red: 6.0/255.0, green: 179.0/255.0, blue: 152.0/255.0, alpha: 1), for: .normal)
+            backgroundColor = UIColor.clear
+            layer.borderWidth = 0.5
+            
+        }else{
+            backgroundColor = UIColor(red: 6.0/255.0, green: 179.0/255.0, blue: 152.0/255.0, alpha: 1)
+            setTitleColor(.white, for: .normal)
+        }
     }
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
