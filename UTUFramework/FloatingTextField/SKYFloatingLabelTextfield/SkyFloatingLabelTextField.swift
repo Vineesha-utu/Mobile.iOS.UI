7451fd5287685bd5fb107c7a966de5d2f5ff6fae
Madhu Kiran Raju <madhu.dasararaju@utu.global>
2020-03-18 18:57:40 +0530

    reverte

--- UTUFramework/FloatingTextField/SKYFloatingLabelTextfield/SkyFloatingLabelTextField.swift
+++ UTUFramework/FloatingTextField/SKYFloatingLabelTextfield/SkyFloatingLabelTextField.swift
@@ -25,7 +25,7 @@ public enum ErrorMessagePlacement {
  A beautiful and flexible textfield implementation with support for title label, error message and placeholder.
  */
 @IBDesignable
-public class SkyFloatingLabelTextField: UITextField , UITextFieldDelegate{ // swiftlint:disable:this type_body_length
+open class SkyFloatingLabelTextField: UITextField , UITextFieldDelegate{ // swiftlint:disable:this type_body_length
     
     let paddingEditing = UIEdgeInsets(top: 5, left: 16, bottom: 5, right: 50)
     var clearBtn : UIButton!
