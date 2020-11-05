df34921e1a8a1a2930aae5f6d60e0d0cc6e6df30
Lakshmana Sattineedi <lakshmana.sattineedi@utu.global>
2020-11-05 16:29:11 +0530

    Moved UIImage extention to UTUSnackbar

--- UTUFramework/Classes/Snackbar/UTUSnackBar.swift
+++ UTUFramework/Classes/Snackbar/UTUSnackBar.swift
@@ -117,6 +117,7 @@ public class UTUSnackBar : NSObject { //MDCSnackbarManagerDelegate
         label.sizeToFit()
         return label.frame.height
     }
+    
     /*public static func showSnackbarOld(title: String, forSuccess : Bool) {
         let actionHandler = {() in
             //todo
@@ -288,3 +289,17 @@ public class UTUSnackBar : NSObject { //MDCSnackbarManagerDelegate
     }
 }
 */
+extension UIImage {
+    func setImageColor(_ color: UIColor) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(size, false, scale)
+        guard let ctx = UIGraphicsGetCurrentContext(), let cgImage = cgImage else { return self }
+        color.setFill()
+        ctx.translateBy(x: 0, y: size.height)
+        ctx.scaleBy(x: 1.0, y: -1.0)
+        ctx.clip(to: CGRect(x: 0, y: 0, width: size.width, height: size.height), mask: cgImage)
+        ctx.fill(CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        guard let colored = UIGraphicsGetImageFromCurrentImageContext() else { return self }
+        UIGraphicsEndImageContext()
+        return colored
+    }
+}
